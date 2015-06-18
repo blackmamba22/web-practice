4519071445784868a724c45cd2ff00d1f2ee317b
unknown <Gawaine_O'Gilvie@CMA-GOgilvie.nuance.com>
2015-06-18 16:10:17 -0400

    Added started files for web development practice blogs

---
+++ jquery-practice/js/app.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+            //document.write("Hello, World!");
+
+
+            $('button').click(function(){
+            	$('div').slideToggle( "slow" );
+            });
+
+
+
+
+ }); // End of document.
