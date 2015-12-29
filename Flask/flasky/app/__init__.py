26cd2d190c4d12ad9cd07969ffa7b72a78ded89b
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-12-29 16:00:58 -0500

    Adding database portion by using slqalchemy flask extension

--- Flask/flasky/app/__init__.py
+++ Flask/flasky/app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'pooch'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
 import views
