c62c2c78a3d9675d2fd58a440b702b1b8224b629
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-12-29 12:25:16 -0500

    completed flask's flash message feature that allows the application to flash a message on any given condition

--- Flask/flasky/app/views.py
+++ Flask/flasky/app/views.py
@@ -1,13 +1,20 @@
 from app import app
 from forms import NameForm
-from flask import render_template, redirect, session, url_for
+from flask import render_template, redirect, session, url_for, flash
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
     if form.validate_on_submit():
+        old_name = session.get('name')
+
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
+
     return render_template('index.html', form=form, name=session.get('name'))
 
 
