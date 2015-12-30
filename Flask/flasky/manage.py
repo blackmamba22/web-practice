9185b0beca1901fceefd8da121ec0a5916af8e23
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-12-30 13:07:57 -0500

    created a database migrations folder and added migration commands in manage.py

--- Flask/flasky/manage.py
+++ Flask/flasky/manage.py
@@ -1,10 +1,13 @@
 from flask.ext.script import Shell, Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db, models
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=models.User, Role=models.Role)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 manager.run()
