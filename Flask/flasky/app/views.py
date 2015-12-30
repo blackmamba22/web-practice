a69fd96851afc9c0d0c258f7c8d49c6f34778a6c
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-12-30 12:00:52 -0500

    updated models with DB changes, modified index to display message if user is known and modified views.py to query database.

--- Flask/flasky/app/views.py
+++ Flask/flasky/app/views.py
@@ -1,9 +1,10 @@
-from app import app
+from app import app, db
 from forms import NameForm
+from models import User
 from flask import render_template, redirect, session, url_for, flash
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+#@app.route('/', methods=['GET', 'POST'])
+def index2():
     form = NameForm()
 
     if form.validate_on_submit():
@@ -16,6 +17,27 @@ def index():
         return redirect(url_for('index'))
 
     return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+
+    return render_template('index.html',
+            form = form, name = session.get('name'),
+            known = session.get('known', False))
 
 
 @app.route('/user/<name>')
