088e0e8487a89cc11822d130146e0598a18120f3
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2016-01-06 20:48:51 -0500

    added blueprint namespace in for the url_for index urls and updated DB table reference

--- Flask/flasky/manage.py
+++ Flask/flasky/manage.py
@@ -10,7 +10,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=models.User, Role=models.Role)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 @manager.command
 def test():
