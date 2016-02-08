fb8c32af1f11988368c0afd407d4696cbf1f7c41
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2016-02-08 10:09:31 -0500

    chapter 8, working on registration and email confirmation

--- Flask/flasky/app/auth/views.py
+++ Flask/flasky/app/auth/views.py
@@ -42,3 +42,15 @@ def register():
         flash('You can now login.')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
