117e012a74eee3ceae0fbb57939afa88c6a42ec5
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2016-02-23 18:10:03 -0500

    added test cases for Question View and Question methods in models.py. Also updated the views.py and models.py functions

--- Django/django_app/polls/views.py
+++ Django/django_app/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,7 +14,10 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        
 
 class DetailView(generic.DetailView):
     model = Question
@@ -22,7 +26,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
