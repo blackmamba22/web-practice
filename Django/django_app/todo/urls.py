c3d184ea78dfd1a32b70f165520854fb2c08780b
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2016-03-12 20:28:30 -0500

    updated urls.py with remove view. added new form to index page to submit removals.

--- Django/django_app/todo/urls.py
+++ Django/django_app/todo/urls.py
@@ -6,4 +6,5 @@ app_name = 'todo'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^remove/$', views.remove_item, name='remove'),
 ]
--- Django/django_app/todo/views.py
+++ Django/django_app/todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 from . import forms, models
 # Create your views here.
@@ -20,3 +21,16 @@ def index(request):
         form = forms.ToDoForm()
 
     return render(request, 'todo/index.html', {'form': form, 'todo': todo_list})
+
+def remove_item(request):
+    if request.method == 'POST':
+        del_item = request.POST.get("remove_button", "nothing found")
+
+        if not del_item == "nothing found":
+            m = models.ToDoItem.objects.get(id=del_item)
+            print m
+            m.delete()
+
+    if 'next' in request.GET:
+        return redirect(request.GET['next'])
+    return render(request, 'todo/base.html')
